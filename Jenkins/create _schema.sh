0e0fc6bf777e3bce914f28870adde7ec2d59cccf
dipti1986 <hello.diptisingh@gmail.com>
2022-03-01 16:20:21 +0000

    removed jenkins file

--- Jenkins/create _schema.sh
+++
@@ -1,5 +0,0 @@
-!#/bin/bash
-source venv/bin/activate
-echo "Creating schema"
-python3 create.py
-echo "schema created"
--- Jenkins/test.sh
+++
@@ -1,8 +0,0 @@
-!#/bin/bash
-
-source venv/bin/activate
-#run pytest
-python3 -m pytest \
-      --cov=application \
-      --cov-report term -missing
-      --cov-report xml:coverage.xml \
